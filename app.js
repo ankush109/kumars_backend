2cacd12f34c797b435281d892ff5d77e0b874647
Ankush Banerjee <banerjeeankush184@gmail.com>
2023-08-18 12:24:59 +0530

    fixed

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const path = require("path")
 const cors = require('cors')
 const app =express()
 const corsOptions ={
-    origin:'http://localhost:3000', 
+    origin:'https://kumars-frontend.vercel.app', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
