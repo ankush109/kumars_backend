a403c78dfb46773f50730309e1efb80d1622c916
Ankush Banerjee <banerjeeankush184@gmail.com>
2023-08-18 12:13:47 +0530

    d

--- utils/jwttoken.js
+++ utils/jwttoken.js
@@ -4,12 +4,12 @@
 const sendtoken =(user,statuscode,res)=>{
     const token = user.getJWTtoken();
     //options for cookie
-    
-    }
+
 res.status(statuscode).cookie("token",token).json({
     success:true,
     token,
     user
 })
-
+;
+}
 module.exports =sendtoken
